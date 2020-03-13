bab765e15bfda23e411c78dba29cf2b2450bc423
vb <bonhomme.vincent@gmail.com>
2020-03-13 14:52:23 +0100

    more tests

--- tests/testthat/test-class-helpers.R
+++ tests/testthat/test-class-helpers.R
@@ -1,4 +1,6 @@
-test_that("class helpers work", {
+test_that("utils work", {
+
+  # classes
   x <- 1
   x2 <- x %>% .append_class("foo")
   expect_equal(x2 %>% class() %>% `[`(1), "foo")
@@ -9,4 +11,8 @@ test_that("class helpers work", {
 
   expect_true(x2 %>% .is_class1("foo"))
   expect_false(x2 %>% .is_class1("numeric"))
+
+  # .seq_naming_list
+  expect_true(list(a=1:5, b=5:2) %>% .seq_naming_list() %>% purrr::map_lgl(~.x %>% names %>% is.null) %>% `!` %>% all)
+  expect_true(list(a=1:5, b=5:2) %>%  purrr::map_lgl(~ .x %>% names %>% is.null) %>% all())
 })
