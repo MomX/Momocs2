d46dd2b5e402fb90d64f04c593dc7b759e691f5a
vb <bonhomme.vincent@gmail.com>
2020-03-13 15:20:49 +0100

    more tests

--- tests/testthat/test-gg.R
+++ tests/testthat/test-gg.R
@@ -5,10 +5,20 @@ test_that("gg0 works", {
 
 test_that("gg works", {
   expect_message(gg("a"))
+  expect_is(bot2 %>% pick %>% gg(), "ggplot")
   # otherwise tested in examples
 })
 
-test_that("gg works", {
-  expect_message(gg("a"))
+test_that("mosaic works", {
+  expect_is(bot2 %>% mosaic, "ggplot")
+  expect_is(bot2 %>% mosaic(type), "ggplot")
+  expect_is(bot2 %>% mosaic(fake, ncol=2), "ggplot")
   # otherwise tested in examples
 })
+
+test_that("pile works", {
+  expect_is(bot2 %>% pile, "ggplot")
+  expect_is(bot2 %>% pile(type), "ggplot")
+  # otherwise tested in examples
+})
+
