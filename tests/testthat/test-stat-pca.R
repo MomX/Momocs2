4a68a4f4dec6a3905b267376816f7cb2f4c430cc
vb <bonhomme.vincent@gmail.com>
2020-03-20 16:46:52 +0100

    Update test-stat-pca.R
    >200 tests now

--- tests/testthat/test-stat-pca.R
+++ tests/testthat/test-stat-pca.R
@@ -1,5 +1,18 @@
+p <- bot2 %>% efourier(4) %>% stat_pca()
+
+
+test_that("stat_pca works", {
+  bf <- bot2 %>% efourier(4)
+  expect_message(bf %>% stat_pca())
+  expect_message(bf %>% stat_pca(center=T))  # hey you when you will have implemented stat_pca.defaukt
+  expect_message(bf %>% stat_pca(scale=T))
+  x <- bf %>% stat_pca(center=TRUE, scale=TRUE)
+  expect_is(x, "tbl")
+  expect_is(x, "pca")
+})
+
 test_that("scree works", {
-  p <- bot2 %>% efourier(4) %>% stat_pca()
+
   expect_equal(scree(p, n_axes=4) %>% nrow(), 4)
   expect_equal(max(scree(p)$cumsum), 1)
 
@@ -9,3 +22,4 @@ test_that("scree works", {
   expect_is(scree_plot(p, 3), "gg")
   expect_is(scree_plot(p, prop=0.5), "gg")
 })
+
