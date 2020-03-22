74295348b69d8539eb47848bd7559c74fdfebce5
vb <bonhomme.vincent@gmail.com>
2020-03-22 14:29:46 +0100

    testing complex to cartesian

--- tests/testthat/test-babel.R
+++ tests/testthat/test-babel.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("complex and cartesian work", {
+  x <- coo_single(tibble::tibble(x=0.213, y=-9.2324))
+  expect_equivalent(x, x %>% cartesian_2_complex() %>% complex_2_cartesian())
 })
