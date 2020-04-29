71a6924553cc751ac59aef65724d03f953fc2bb0
vb <bonhomme.vincent@gmail.com>
2020-04-29 12:12:23 +0200

    forgot to rm examples for now

--- R/core_dfourier.R
+++ R/core_dfourier.R
@@ -27,31 +27,7 @@
 #'
 #' @family morphometrics
 #' @examples
-#' \dontrun{ # because it's long
-#' od <- dfourier(olea)
-#' od
-#' op <- PCA(od)
-#' plot(op, 1)
-#' }
-#' # dfourier and inverse dfourier
-#' o <- olea[1]
-#' o <- coo_bookstein(o)
-#' coo_plot(o)
-#' o.dfourier <- dfourier(o, nb.h=12)
-#' o.dfourier
-#' o.i <- dfourier_i(o.dfourier)
-#' o.i <- coo_bookstein(o.i)
-#' coo_draw(o.i, border='red')
-#'
-#' #future calibrate_reconstructions
-#' o <- olea[1]
-#' h.range <- 2:13
-#' coo <- list()
-#' for (i in seq(along=h.range)){
-#' coo[[i]] <- dfourier_i(dfourier(o, nb.h=h.range[i]))}
-#' names(coo) <- paste0('h', h.range)
-#' panel(Opn(coo), borders=col_india(12), names=TRUE)
-#' title('Discrete Cosine Transforms')
+#' olea[1:3, ] %>% dfourier()
 #' @export
 dfourier <- function(x, nb_h, raw=FALSE, from_col, to_col, drop_coo, ...) {
   UseMethod("dfourier")
